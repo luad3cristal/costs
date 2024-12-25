a69433f18e211d32eb942485e7c6ef71ed20ee5b
Luana Lima <luanalima.2006@hotmail.com>
2024-12-25 11:48:07 -0300

    feat: add and start styling dashboard

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,6 +3,8 @@ import { useLocation } from "react-router-dom";
 import S from "./Projects.module.css";
 
 import Message from "../layout/message";
+import Container from "../layout/container";
+import LinkButton from "../layout/linkButton";
 
 function Projects() {
   const location = useLocation();
@@ -12,9 +14,16 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
+    <div className={S.project_container}>
+      <div className={S.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
